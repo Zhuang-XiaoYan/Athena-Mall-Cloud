388cde84c54a6cfa0fe3b0baf34ee795ebb3c8e1
xjl <18279148786@163.com>
2022-01-23 16:49:50 +0800

    feat(Athena) [xjl003] Athena project service build

---
+++ Athena-coupon/src/test/java/com/zhuangxiaoyan/athena/coupon/AthenaCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuangxiaoyan.athena.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Athena-product/src/test/java/com/zhuangxioayan/athena/product/AthenaProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuangxioayan.athena.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
