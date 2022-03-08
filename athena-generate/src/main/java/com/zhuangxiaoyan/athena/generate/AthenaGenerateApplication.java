1f51253bf27f3e70f899bd7585d8b248d66a027a
xjl <18279148786@163.com>
2022-03-08 21:50:22 +0800

    feat(Athena) [xjl026] update Athena-generate project set up

---
+++ athena-generate/src/main/java/com/zhuangxiaoyan/athena/generate/AthenaGenerateApplication.java
@@ -0,0 +1,17 @@
+package com.zhuangxiaoyan.athena.generate;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@MapperScan("com.zhuangxiaoyan.athena.generate.dao")
+public class AthenaGenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaGenerateApplication.class, args);
+    }
+
+}
---
+++ athena-generate/src/test/java/com/zhuangxiaoyan/athena/generate/AthenaGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuangxiaoyan.athena.generate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaGenerateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
