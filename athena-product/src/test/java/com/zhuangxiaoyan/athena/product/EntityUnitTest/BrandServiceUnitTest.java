51110506648880fa8d91d7b42ba2d68ff0e0d274
xjl <18279148786@163.com>
2022-03-09 23:20:23 +0800

    feat(Athena) [xjl033] add java code to athena-producter service

--- athena-product/src/test/java/com/zhuangxiaoyan/athena/product/EntityUnitTest/BrandServiceUnitTest.java
+++ athena-product/src/test/java/com/zhuangxiaoyan/athena/product/EntityUnitTest/BrandServiceUnitTest.java
@@ -1,4 +1,4 @@
-package com.zhuangxiaoyan.athena.product;
+package com.zhuangxiaoyan.athena.product.EntityUnitTest;
 
 import com.zhuangxiaoyan.athena.product.entity.BrandEntity;
 import com.zhuangxiaoyan.athena.product.service.BrandService;
@@ -9,24 +9,41 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * @description 单元测试中的两个的注解是原理是什么？如果没有会出现什么错误。为什么会出现的这样的错误。
- * @param: null
- * @date: 2022/3/9 22:43
- * @return:
- * @author: xjl
+ * @Classname BrandServiceUnitTest
+ * @Description BrandService 单元测试类
+ * @Date 2022/3/9 22:47
+ * @Created by xjl
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class AthenaProductApplicationTests {
+public class BrandServiceUnitTest {
+
     @Autowired
     BrandService brandService;
 
+
     @Test
-    void contextLoads() {
+    void save(){
         BrandEntity brandEntity = new BrandEntity();
         brandEntity.setName("庄小焱");
         brandEntity.setDescript("test service");
         brandService.save(brandEntity);
         System.out.println("保存成功……");
     }
+
+    @Test
+    void update(){
+
+    }
+
+    @Test
+    void find(){
+
+    }
+
+    @Test
+    void delete(){
+
+    }
+
 }
