1411b910271bd5dd6381a5541caaf143a7da0012
jiale_xiong <jiale_xiong@dell.com>
2022-03-10 11:31:18 +0800

    feat(Athena) [xjl025] success set up athena-coupon system but not finish unittest

--- athena-coupon/src/main/java/com/zhuangxiaoyan/athena/coupon/AthenaCouponApplication.java
+++ athena-coupon/src/main/java/com/zhuangxiaoyan/athena/coupon/AthenaCouponApplication.java
@@ -1,8 +1,10 @@
 package com.zhuangxiaoyan.athena.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.zhuangxiaoyan.athena.coupon.dao")
 @SpringBootApplication
 public class AthenaCouponApplication {
 
