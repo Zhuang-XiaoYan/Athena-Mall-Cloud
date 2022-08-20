b8e985b4e5044ede24ecf49ad2d922dda721df4a
xjl <18279148786@163.com>
2022-08-21 00:01:52 +0800

    feat(Athena) [xjl164] 新增athena-index首页项目。

---
+++ athena-index/src/main/java/com/zhuangxiaoyan/athena/index/AthenaIndexApplication.java
@@ -0,0 +1,13 @@
+package com.zhuangxiaoyan.athena.index;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaIndexApplication.class, args);
+    }
+
+}
