0cb4baf0b729b21433be677204f8a0a1979fdfa8
xjl <18279148786@163.com>
2022-09-01 21:17:42 +0800

    feat(Athena) [xjl177] 添加用户的退出功能

---
+++ athena-sso/src/main/java/com/zhuangxiaoyan/athena/sso/web/LogoutController.java
@@ -0,0 +1,24 @@
+package com.zhuangxiaoyan.athena.sso.web;
+
+import com.zhuangxiaoyan.common.utils.Result;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Classname LogoutController
+ * @Description TODO
+ * @Date 2022/9/1 21:15
+ * @Created by xjl
+ */
+@Controller
+public class LogoutController {
+
+    @GetMapping("/logout")
+    public Result logout(){
+        // 删除的redis的相关数据
+
+        // 删除当前的cookie的数据
+
+        return Result.ok();
+    }
+}
