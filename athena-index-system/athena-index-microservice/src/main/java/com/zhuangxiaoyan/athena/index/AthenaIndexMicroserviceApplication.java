6bbca600dadc5e90b9ab1344f43915095127fadf
xjl <18279148786@163.com>
2022-09-27 23:37:32 +0800

    feat(AthenaMall) [xjl193] 构建了athena-index 系统的项目结构。

---
+++ athena-index-system/athena-index-microservice/src/main/java/com/zhuangxiaoyan/athena/index/AthenaIndexMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.zhuangxiaoyan.athena.index;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaIndexMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaIndexMicroserviceApplication.class, args);
+    }
+
+}
---
+++ athena-product-system/athena-product-mircoservice/src/main/java/com/zhuangxiaoyan/athena/product/AthenaProductMircoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.zhuangxiaoyan.athena.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaProductMircoserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaProductMircoserviceApplication.class, args);
+    }
+
+}
