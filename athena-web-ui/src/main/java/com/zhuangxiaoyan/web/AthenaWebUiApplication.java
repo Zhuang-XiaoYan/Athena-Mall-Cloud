e7e5039e0fe5eae147993cdb9d64f90f93d6a424
xjl <18279148786@163.com>
2022-10-27 15:29:57 +0800

    feat(AthenaMall) [xjl203] 构建的athena-web-ui 系统，将所有的页面全部统一放在一个服务。

---
+++ athena-web-ui/src/main/java/com/zhuangxiaoyan/web/AthenaWebUiApplication.java
@@ -0,0 +1,13 @@
+package com.zhuangxiaoyan.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaWebUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaWebUiApplication.class, args);
+    }
+
+}
---
+++ athena-web-ui/src/test/java/com/zhuangxiaoyan/web/AthenaWebUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuangxiaoyan.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaWebUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
