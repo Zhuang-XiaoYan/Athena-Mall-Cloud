45bda10d12f928a788ba6d55227646e8adae616c
xjl <18279148786@163.com>
2022-10-28 08:33:52 +0800

    feat(AthenaMall) [xjl208] 添加athena-author系统权限服务。

---
+++ athena-author/src/test/java/com/zhuangxiaoyan/author/AthenaAuthorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuangxiaoyan.author;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaAuthorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
